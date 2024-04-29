bc707c43f4de33180ff96d137fde5ca84aada0a7
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-04-29 12:42:56 +0700

    - create base template

--- django_template/accounts/views.py
+++ django_template/accounts/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 
 class SampleView(TemplateView):
-    template_name = "base.html"
+    template_name = "accounts/sample_html.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
